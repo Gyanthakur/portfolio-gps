20c7f7b7ab204531f14b47811868ab30c9baec8d
gyan pratap singh <98226958+Gyanthakur@users.noreply.github.com>
2023-10-22 13:40:48 +0530

    Revert "Fix footer hidden"

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:["","Web Designer","Problem Solver","Frontend Developer","Backend Developer"],
+    strings:["","Web Desiner","Problem Solver","Frontend Devloper","Backend Devloper"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
