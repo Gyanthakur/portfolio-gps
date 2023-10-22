6f955dd570c29ef2e78c24255e04c8299faf086e
Logesh-waran2003 <logeshwaransrini@gmail.com>
2023-10-22 11:15:03 +0530

    fix/funcionality of back-to-top button

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ var typed = new Typed(".text",{
     BackSpeed:60,
     loop:true
 })
+
+// back to top button
+
+// Get the button:
+let mybutton = document.getElementById("backToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
